6a05df2daa200af6e3bea4b24ccae7c9f4a6039c
Leo <leo.chen@cloud-interactive.com>
2020-09-03 16:03:53 +0800

    Update 04 folder

--- 04/index.js
+++ 04/index.js
@@ -1,4 +1,9 @@
 "use strict";
+// function
+// 隱性 any (Implicit Any)
+// 大部分的情況下，只要定義任何函式 TypeScript 通常會無條件推論函式內的參數（Parameters）為 any 型別
+// 推論與註記
+// 分別為輸入參數與輸出部分，大部分情況下，只要我們提供函式參數的註記，輸出就可以間接被 TypeScript 推論出來
 var say = function () {
     console.log('hi');
 };
