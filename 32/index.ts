4ce41bea6d1be5682ce13efbd6d8b1d1b0823280
Leo <leo.chen@cloud-interactive.com>
2020-09-11 17:26:02 +0800

    Add 32 folder

---
+++ 32/index.ts
@@ -0,0 +1,10 @@
+// tsconfig.js II
+
+/*
+
+outDir:         指定檔案輸出位置
+rootDir:        指定編譯範圍 (如果超出 rootDir 指定的範圍，並且沒有啟用 noEmitOnError，照樣會編譯出結果)
+noEmitOnError:  只要有環節出錯，是否不輸出
+outFile:        將專案打包成單一檔案 (限制：module 必須為 amd or system)
+
+*/
