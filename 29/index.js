6c50c5eec815c9330a507306820a70e7c758dff2
Leo <leo.chen@cloud-interactive.com>
2020-09-11 14:57:53 +0800

    Update 29 folder

--- 29/index.js
+++ 29/index.js
@@ -14,6 +14,9 @@ Factory Method
 
 Abstract Factory Pattern
 
+若想建立的不同物件，每種不同物件差異性比較大，但又是隸屬於同一種類型 C，
+可以宣告 C 的抽象格式（根據情境使用介面或抽象類別），並且讓不同物件類型繼承自 C（若 C 為抽象類別）或與 C 進行綁定（若 C 為介面）
+
 不用直接實踐出實體工廠，而是藉由介面或抽象類別先進行抽象化工廠的動作，再分別延伸出想要創建的實體工廠
 
 以本篇舉的例子來說，儘管武器 Weapon 跟防具 Armour 是兩個完全不ㄧ樣的物件，但同時又為角色裝備的類型 Equipment
@@ -21,6 +24,7 @@ Abstract Factory Pattern
 
 且當 Equipment 確定建好之後，就可以建立相對應的 EquipmentFactory 抽象工廠，
 並延伸出不同職業的裝備工廠 (SwordsmanEquipmentFactory && WarlockEquipmentFactory)
+
 */
 var Swordsman_1 = __importDefault(require("./profession/Swordsman"));
 var Warlock_1 = __importDefault(require("./profession/Warlock"));
